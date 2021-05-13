2c226c956dee4002b7afad8b8645614661da51e8
Leonardo Guedes <leoguedes.14@hotmail.com>
2021-05-13 00:44:00 -0300

    added modal image view

--- src/main.js
+++ src/main.js
@@ -1,50 +1,55 @@
-let buttonsFilter = document.getElementById('my-btn-container').children;
-let allCards = document.getElementsByClassName('row')
-let arrayAllCards = Array.from(allCards[0].children)
+let buttonsFilter = [...document.getElementById('btn-container').children];
+let allCards = [...document.getElementsByClassName('flexcontainer').item(0).children]
 let modal = document.getElementById('modal-container')
 let modalCotentImg = document.getElementById('modal-content-img')
 let ButtonCloseModal = document.getElementById('btn-close')
 let divCardCurrentOpened = null
 
 async function filterSelection(type, btn) {
-    let showAll = arrayAllCards.forEach(div => div.style.display = "block")
+    let showAllCards = allCards.forEach(card => card.style.display = "block")
     let isBtnFilterActive = btn.className.includes('active')
     let btnRemoveClassActive = btn => btn.className = 'btn'
-    let allDivsWithoutId = div => !div.className.includes(type)
+    let allDivsWithoutType = card => !card.className.includes(type)
 
     if (isBtnFilterActive) {
-        showAll
+        showAllCards
         btnRemoveClassActive(btn)
     } else {
         if (type === 'showAll') {
-            showAll
+            showAllCards
         } else {
-            arrayAllCards.filter(div => {
-                if (allDivsWithoutId(div)) {
-                    return div.style.display = "none"
+            allCards.filter(card => {
+                if (allDivsWithoutType(card)) {
+                    return card.style.display = "none"
                 }
 
             })
         }
-        Array.from(buttonsFilter).forEach(btnRemoveClassActive)
+        buttonsFilter.forEach(btnRemoveClassActive)
         btn.className = 'btn active'
     }
 }
+const toggleHoverCard = () => divCardCurrentOpened.classList.toggle('noHover')
+const toggleModalClassActive = () => modal.classList.toggle('modal_active')
+const getSrcCard = card => card.children[0].children[0].src
 
-const addEventCLickToAllCards = div => {
-    div.addEventListener('click', () => {
-        let srcClickedImg = div.children[0].children[0].src
-        modalCotentImg.setAttribute('src', srcClickedImg)
-        modal.classList.toggle('modal_active')
-        divCardCurrentOpened = div;
-        divCardCurrentOpened.classList.toggle('noHover')
+
+const addEventCLickToAllCards = card => {
+    card.addEventListener('click', () => {
+        divCardCurrentOpened = card; // save current card opened for restore hover after.
+        toggleHoverCard()
+        modalCotentImg.setAttribute('src', getSrcCard(card))
+        toggleModalClassActive()
+
+
     })
 }
 
 const eventCloseAndRestoreModal = () => {
-    divCardCurrentOpened.classList.toggle('noHover')
-    modal.classList.toggle('modal_active')
+    modalCotentImg.removeAttribute('src')
+    toggleHoverCard() // restore effect hover to card
+    toggleModalClassActive()
 }
 
-arrayAllCards.forEach(addEventCLickToAllCards)
+allCards.forEach(addEventCLickToAllCards)
 ButtonCloseModal.addEventListener('click', eventCloseAndRestoreModal)
